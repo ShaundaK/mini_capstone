897fc972a1f30ddf8213acd01aebea758d7d1be0
ShaundaK <sdking1913@gmail.com>
2018-10-15 21:52:13 -0500

    runner works with index and show actions

--- runner.rb
+++ runner.rb
@@ -1,5 +1,7 @@
-# require 'unirest'
+require 'unirest'
+require 'awesome_print'
 
-# response = Unirest.post("localhost:3000/api/products")
+# response = Unirest.get("localhost:3000/api/products")
+response = Unirest.get("localhost:3000/api/products/2")
 
-# p response.body
+ap response.body
