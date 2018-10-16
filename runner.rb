25508c1a710bfe0c7f4330a3fa5d4237f4ef5713
ShaundaK <sdking1913@gmail.com>
2018-10-15 22:02:08 -0500

    create action works

--- runner.rb
+++ runner.rb
@@ -2,6 +2,8 @@ require 'unirest'
 require 'awesome_print'
 
 # response = Unirest.get("localhost:3000/api/products")
-response = Unirest.get("localhost:3000/api/products/2")
+# response = Unirest.get("localhost:3000/api/products/2")
+response = Unirest.post("localhost:3000/api/products")
+
 
 ap response.body
